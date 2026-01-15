a7c700be69491962b2186f3dcc5e1a43e3c818f9
jco <jacobtco@gmail.com>
2026-01-15 20:31:37 +0800

    feat: added tests

---
+++ go-server/hello.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Hello(name string) string {
+	if name == "" {
+		return "Hello World"
+	}
+
+	return "Hello, " + name
+}
+
+func main() {
+	fmt.Print(Hello("Jacob"))
+}
